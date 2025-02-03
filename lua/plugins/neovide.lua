814a3093e31fe05887f30d190f58b67f97d1d93f
SourishBiswas23 <sourish135@gmail.com>
2025-02-03 23:32:05 +0530

    Fixed the copy and paste issue in neovide

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -1,5 +1,8 @@
 if vim.g.neovide then
   vim.opt.linespace = 1.0
+  vim.api.nvim_set_keymap("v", "<sc-c>", '"+y', { noremap = true }) -- Select line(s) in visual mode and copy (CTRL+Shift+V)
+  vim.api.nvim_set_keymap("i", "<sc-v>", '<ESC>"+p', { noremap = true }) -- Paste in insert mode (CTRL+Shift+C)
+  vim.api.nvim_set_keymap("n", "<sc-v>", '"+p', { noremap = true }) -- Paste in normal mode (CTRL+Shift+C)
 end
 
 return {}
