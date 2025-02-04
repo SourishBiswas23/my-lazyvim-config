e7cd7bb332b1cbf93fcbb1067a119c2179cbc6e4
SourishBiswas23 <sourish135@gmail.com>
2025-02-04 12:33:09 +0530

    Using vtsls as a replacement for ts_ls

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,11 +1,11 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    local servers = {
+  opts = {
+    servers = {
       volar = {
         init_options = {
           vue = {
-            hybridMode = false,
+            hybridMode = true,
           },
         },
       },
@@ -48,10 +48,116 @@ return {
         },
       },
       ts_ls = {
-        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+        enabled = false,
       },
-    }
-    local setup = {
+      tsserver = {
+        enabled = false,
+      },
+      vtsls = {
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+          "vue",
+        }, -- Ensure "vue" is included
+        settings = {
+          vtsls = {
+            -- for vue
+            tsserver = {
+              globalPlugins = {
+                {
+                  name = "@vue/typescript-plugin",
+                  location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
+                  languages = { "vue" },
+                  configNamespace = "typescript",
+                  enableForWorkspaceTypeScriptVersions = true,
+                },
+              },
+            },
+            -- for ts
+            enableMoveToFileCodeAction = true,
+            autoUseWorkspaceTsdk = true,
+            experimental = {
+              maxInlayHintLength = 30,
+              completion = {
+                enableServerSideFuzzyMatch = true,
+              },
+            },
+          },
+          -- for ts
+          typescript = {
+            updateImportsOnFileMove = { enabled = "always" },
+            suggest = {
+              completeFunctionCalls = true,
+            },
+            inlayHints = {
+              enumMemberValues = { enabled = true },
+              functionLikeReturnTypes = { enabled = true },
+              parameterNames = { enabled = "literals" },
+              parameterTypes = { enabled = true },
+              propertyDeclarationTypes = { enabled = true },
+              variableTypes = { enabled = false },
+            },
+          },
+        },
+        keys = {
+          {
+            "gD",
+            function()
+              local params = vim.lsp.util.make_position_params()
+              LazyVim.lsp.execute({
+                command = "typescript.goToSourceDefinition",
+                arguments = { params.textDocument.uri, params.position },
+                open = true,
+              })
+            end,
+            desc = "Goto Source Definition",
+          },
+          {
+            "gR",
+            function()
+              LazyVim.lsp.execute({
+                command = "typescript.findAllFileReferences",
+                arguments = { vim.uri_from_bufnr(0) },
+                open = true,
+              })
+            end,
+            desc = "File References",
+          },
+          {
+            "<leader>co",
+            LazyVim.lsp.action["source.organizeImports"],
+            desc = "Organize Imports",
+          },
+          {
+            "<leader>cM",
+            LazyVim.lsp.action["source.addMissingImports.ts"],
+            desc = "Add missing imports",
+          },
+          {
+            "<leader>cu",
+            LazyVim.lsp.action["source.removeUnused.ts"],
+            desc = "Remove unused imports",
+          },
+          {
+            "<leader>cD",
+            LazyVim.lsp.action["source.fixAll.ts"],
+            desc = "Fix all diagnostics",
+          },
+          {
+            "<leader>cV",
+            function()
+              LazyVim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
+            end,
+            desc = "Select TS workspace version",
+          },
+        },
+      },
+    },
+    setup = {
       tailwindcss = function(_, opts)
         local tw = LazyVim.lsp.get_raw_config("tailwindcss")
         opts.filetypes = opts.filetypes or {}
@@ -79,22 +185,66 @@ return {
         vim.list_extend(opts.filetypes, opts.filetypes_include or {})
       end,
       ts_ls = function(_, opts)
-        local mason_registry = require("mason-registry")
-        local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
-          .. "/node_modules/@vue/language-server"
-        local init_options = {
-          plugins = {
-            {
-              name = "@vue/typescript-plugin",
-              location = vue_language_server_path,
-              languages = { "vue" },
-            },
-          },
-        }
-        opts.init_options = init_options
+        return true
       end,
-    }
-    opts.servers = servers
-    opts.setup = setup
-  end,
+      tsserver = function()
+        return true
+      end,
+      vtsls = function(_, opts)
+        LazyVim.lsp.on_attach(function(client, buffer)
+          client.commands["_typescript.moveToFileRefactoring"] = function(command, ctx)
+            ---@type string, string, lsp.Range
+            local action, uri, range = unpack(command.arguments)
+
+            local function move(newf)
+              client.request("workspace/executeCommand", {
+                command = command.command,
+                arguments = { action, uri, range, newf },
+              })
+            end
+
+            local fname = vim.uri_to_fname(uri)
+            client.request("workspace/executeCommand", {
+              command = "typescript.tsserverRequest",
+              arguments = {
+                "getMoveToRefactoringFileSuggestions",
+                {
+                  file = fname,
+                  startLine = range.start.line + 1,
+                  startOffset = range.start.character + 1,
+                  endLine = range["end"].line + 1,
+                  endOffset = range["end"].character + 1,
+                },
+              },
+            }, function(_, result)
+              ---@type string[]
+              local files = result.body.files
+              table.insert(files, 1, "Enter new path...")
+              vim.ui.select(files, {
+                prompt = "Select move destination:",
+                format_item = function(f)
+                  return vim.fn.fnamemodify(f, ":~:.")
+                end,
+              }, function(f)
+                if f and f:find("^Enter new path") then
+                  vim.ui.input({
+                    prompt = "Enter move destination:",
+                    default = vim.fn.fnamemodify(fname, ":h") .. "/",
+                    completion = "file",
+                  }, function(newf)
+                    return newf and move(newf)
+                  end)
+                elseif f then
+                  move(f)
+                end
+              end)
+            end)
+          end
+        end, "vtsls")
+        -- copy typescript settings to javascript
+        opts.settings.javascript =
+          vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
+      end,
+    },
+  },
 }
