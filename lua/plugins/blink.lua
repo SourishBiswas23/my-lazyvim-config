cc6de02bacaff8f71a9dece8b976bab33f00129b
SourishBiswas23 <sourish135@gmail.com>
2025-02-04 13:07:31 +0530

    Removed ghost text from blink.cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -4,6 +4,9 @@ return {
     completion = {
       menu = { border = "rounded" },
       documentation = { window = { border = "rounded" } },
+      ghost_text = {
+        enabled = false,
+      },
     },
     signature = { window = { border = "rounded" } },
     keymap = {
