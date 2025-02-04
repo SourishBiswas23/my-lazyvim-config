d813f0ac1756fcff115f8cc9da1966fb3f758cf7
SourishBiswas23 <sourish135@gmail.com>
2025-02-04 12:41:48 +0530

    Added yaml support

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,39 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      yamlls = {
+        -- Have to add this for yamlls to understand that we support line folding
+        capabilities = {
+          textDocument = {
+            foldingRange = {
+              dynamicRegistration = false,
+              lineFoldingOnly = true,
+            },
+          },
+        },
+        -- lazy-load schemastore when needed
+        on_new_config = function(new_config)
+          new_config.settings.yaml.schemas =
+            vim.tbl_deep_extend("force", new_config.settings.yaml.schemas or {}, require("schemastore").yaml.schemas())
+        end,
+        settings = {
+          redhat = { telemetry = { enabled = false } },
+          yaml = {
+            keyOrdering = false,
+            format = {
+              enable = true,
+            },
+            validate = true,
+            schemaStore = {
+              -- Must disable built-in schemaStore support to use
+              -- schemas from SchemaStore.nvim plugin
+              enable = false,
+              -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+              url = "",
+            },
+          },
+        },
+      },
       volar = {
         init_options = {
           vue = {
@@ -61,7 +94,7 @@ return {
           "typescript",
           "typescriptreact",
           "typescript.tsx",
-          "vue",
+          "v e",
         }, -- Ensure "vue" is included
         settings = {
           vtsls = {
@@ -244,6 +277,14 @@ return {
         -- copy typescript settings to javascript
         opts.settings.javascript =
           vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
+      end,
+      yamlls = function()
+        -- Neovim < 0.10 does not have dynamic registration for formatting
+        if vim.fn.has("nvim-0.10") == 0 then
+          LazyVim.lsp.on_attach(function(client, _)
+            client.server_capabilities.documentFormattingProvider = true
+          end, "yamlls")
+        end
       end,
     },
   },
