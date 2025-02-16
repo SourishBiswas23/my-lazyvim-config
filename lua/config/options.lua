20a8bd73311abc57ea8a031cc13b7fc3bf7a7b2a
SourishBiswas23 <sourish135@gmail.com>
2025-02-16 13:23:57 +0530

    Enabled Line Wrapping

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,15 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
 vim.g.snacks_animate = false
-vim.opt.clipboard = ""
+opt.clipboard = ""
+
+-- Line wrapping
+opt.textwidth = 0 -- Disable automatic line wrapping based on text width
+opt.wrapmargin = 0 -- Disable automatic line wrapping based on the wrap margin
+opt.wrap = true -- Enable line wrapping
+opt.linebreak = true -- Wrap lines at word boundaries rather than in the middle of a word
+opt.textwidth = 80 -- Set maximum text width for automatic line wrapping to 80 characters
+opt.formatoptions = vim.opt.formatoptions + "t" -- Automatically wrap text at 'textwidth' while typing
