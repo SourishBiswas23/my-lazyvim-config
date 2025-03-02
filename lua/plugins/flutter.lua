b4f2076ff2421bef03b52e015a8622cb38f60032
SourishBiswas23 <sourish135@gmail.com>
2025-03-02 13:15:55 +0530

    Flutter tools now showing colors

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -11,6 +11,16 @@ return {
         enabled = true,
       },
       flutter_path = "/usr/bin/flutter",
+      lsp = {
+        color = {
+          enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+          background = true, -- highlight the background
+          background_color = { r = 0, g = 0, b = 0 }, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+          foreground = false, -- highlight the foreground
+          virtual_text = false, -- show the highlight using virtual text
+          virtual_text_str = "■", -- the virtual text character to highlight
+        },
+      },
     })
   end,
 }
