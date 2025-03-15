1a58b268118e876c3a8f1b9a9c5ff6c91cc3bc2e
SourishBiswas23 <sourish135@gmail.com>
2025-03-15 21:53:38 +0530

    Added borders to lazy window

--- lua/plugins/nvimLspconfig.lua
+++ lua/plugins/nvimLspconfig.lua
@@ -1,6 +1,18 @@
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
+    local _border = "rounded"
+
+    local handlers = {
+      ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+      ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+    }
+
+    -- Do not forget to use the on_attach function
+    local lspconfig = require("lspconfig")
+    lspconfig.dartls.setup({ handlers = handlers })
+    lspconfig.lua_ls.setup({ handlers = handlers })
+
     opts.diagnostics.virtual_text = false
     opts.inlay_hints.enabled = false
   end,
