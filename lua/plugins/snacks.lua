f929bc2ab28437868627eb99f51b4c857b4f3280
SourishBiswas23 <sourish135@gmail.com>
2025-03-29 16:29:50 +0530

    Added relative numbering in the sidebar

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,4 +1,5 @@
 return {
   "folke/snacks.nvim",
   keys = { { "<leader>/", false } },
+  opts = { picker = { sources = { explorer = { win = { list = { wo = { relativenumber = true } } } } } } },
 }
